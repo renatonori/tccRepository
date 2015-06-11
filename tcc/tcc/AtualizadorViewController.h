3001114dd79d0e281675207275301f84640ca956
renatoioshida <renato.ioshida@mobwise.com.br>
2015-06-11 17:32:27 -0300

    Layout

---
+++ tcc/tcc/AtualizadorViewController.h
@@ -0,0 +1,13 @@
+//
+//  AtualizadorViewController.h
+//  tcc
+//
+//  Created by Renato Ioshida on 11/06/15.
+//  Copyright (c) 2015 Renato Ioshida. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AtualizadorViewController : UIViewController
+
+@end
---
+++ tcc/tcc/AtualizadorViewController.m
@@ -0,0 +1,38 @@
+//
+//  AtualizadorViewController.m
+//  tcc
+//
+//  Created by Renato Ioshida on 11/06/15.
+//  Copyright (c) 2015 Renato Ioshida. All rights reserved.
+//
+
+#import "AtualizadorViewController.h"
+
+@interface AtualizadorViewController ()
+
+@end
+
+@implementation AtualizadorViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self.navigationItem setHidesBackButton:YES animated:NO];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
