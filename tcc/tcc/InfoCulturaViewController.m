d159ce98a0b7914c4e707b323ba241db69f07f44
renatoioshida <renato.ioshida@mobwise.com.br>
2015-10-05 14:41:25 -0300

    - teste arrumado

--- tcc/tcc/InfoCulturaViewController.m
+++ tcc/tcc/InfoCulturaViewController.m
@@ -17,7 +17,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [self.imagemCulturaImageView setImageWithURL:[NSURL URLWithString:@"http://revistaagrosa.com.br/site/wp-content/uploads/2015/05/SOJA.jpg"] placeholderImage:ni];
+    [self.imagemCulturaImageView setImageWithURL:[NSURL URLWithString:@"http://revistaagrosa.com.br/site/wp-content/uploads/2015/05/SOJA.jpg"] placeholderImage:nil];
     // Do any additional setup after loading the view.
 }
 
